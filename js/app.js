730c29053aa45c842a860b0a61b6d6831eb88c9d
Jackdes <phuong.binh0310@gmail.com>
2019-03-25 01:17:32 +0700

    Final

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
+var btn_scroll = document.getElementById("block-btn-scroll");
 window.onload = function() {
-    console.log('Ready');
+    btn_scroll.addEventListener("click", function() {
+        window.scroll({
+            top: 0,
+            behavior: 'smooth'
+        });
+         
+    });
+    
+    window.addEventListener('scroll', function() {
+        var position_animation = window.scrollY;
+        var size_width_screen = window.screen.width;
+        
+        if ( position_animation >= 20) { 
+            btn_scroll.style.display = 'flex';
+        } else {
+            btn_scroll.style.display = 'none';
+        }
+    });
 }
